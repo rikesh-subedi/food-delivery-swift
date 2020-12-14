27d32597fa2b0586c78f74d5f85fb8ef1ae9324b
Rikesh Subedi <rikesh.subedi@sap.com>
2020-12-14 09:38:08 +0545

    first commit

---
+++ FoodDelivery/FoodDeliveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodDeliveryApp.swift
+//  FoodDelivery
+//
+//  Created by Subedi, Rikesh on 11/12/20.
+//
+
+import SwiftUI
+
+@main
+struct FoodDeliveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeScreenModule().build()
+        }
+    }
+}
